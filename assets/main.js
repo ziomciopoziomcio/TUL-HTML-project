913d94b30dce11a6250c56bc8ea0c7ddb3c65873
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2024-04-22 14:39:13 +0200

    JS dla zapisow, todo

--- assets/main.js
+++ assets/main.js
@@ -6,3 +6,33 @@ document.getElementById('navbar-button').addEventListener('click', function() {
         navbarContainer.style.display = 'none';
     }
 });
+
+//TODO: Fix
+
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+
+    var emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if (!emailPattern.test(email)) {
+        alert('Proszę wprowadzić poprawny adres e-mail.');
+        return;
+    }
+
+    var phonePattern = /^(\+)?[0-9]{9,11}$/;
+    if (!phonePattern.test(phone)) {
+        alert('Proszę wprowadzić poprawny numer telefonu.');
+        return;
+    }
+
+    var inputs = document.querySelectorAll('input, select');
+    for (var i = 0; i < inputs.length; i++) {
+        if (!inputs[i].value) {
+            alert('Proszę wypełnić wszystkie pola.');
+            return;
+        }
+    }
+    alert('Dane zostały poprawnie przesłane.');
+});
